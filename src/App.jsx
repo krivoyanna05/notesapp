73a76457a411a99c9b3ab0da2b722224dfa6f7a5
Anna <krivoy.3@buckeyemail.osu.edu>
2026-09-20 14:12:10 -0400

    remove duplicate imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,6 @@
-import { useState } from 'react'
-import heroImg from './assets/hero.png'
 import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
 import './App.css'
-import reactLogo from "./assets/react.svg";
-import "./App.css";
+
 function App() {
   return (
     <div className="App">
@@ -15,4 +11,5 @@ function App() {
     </div>
   );
 }
+
 export default App;
